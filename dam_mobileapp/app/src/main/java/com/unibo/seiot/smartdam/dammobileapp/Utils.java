e1c8dbdec72e4e56a7c6568b09db4348ca409245
andredecre <73247775+andredecre@users.noreply.github.com>
2021-02-11 12:13:40 +0100

    Add mobile app (definitely not copied)

---
+++ dam_mobileapp/app/src/main/java/com/unibo/seiot/smartdam/dammobileapp/Utils.java
@@ -0,0 +1,11 @@
+package com.unibo.seiot.smartdam.dammobileapp;
+
+public class Utils {
+    public static final String APP_LOG_TAG = "BT CLN";
+
+    public class bluetooth {
+        public static final int ENABLE_BT_REQUEST = 1;
+        public static final String BT_DEVICE_ACTING_AS_SERVER_NAME = "isi21"; //MODIFICARE QUESTA COSTANTE CON IL NOME DEL DEVICE CHE FUNGE DA SERVER
+        public static final String BT_SERVER_UUID = "7ba55436-01eb-11e9-8nz2-f2801f1b9fd1";
+    }
+}
