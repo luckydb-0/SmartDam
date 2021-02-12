55d448132dbd5d9043574cb37bc6a6db288c1d20
andredecre <73247775+andredecre@users.noreply.github.com>
2021-02-12 13:14:57 +0100

    Upgrade mobile app

--- dam_mobileapp/app/src/main/java/com/unibo/seiot/smartdam/dammobileapp/MainActivity.java
+++ dam_mobileapp/app/src/main/java/com/unibo/seiot/smartdam/dammobileapp/MainActivity.java
@@ -6,13 +6,22 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 import unibo.btlib.BluetoothChannel;
@@ -25,6 +34,9 @@ import unibo.btlib.exceptions.BluetoothDeviceNotFound;
 public class MainActivity extends AppCompatActivity {
 
     private BluetoothChannel btChannel;
+    private boolean manualMode = false;
+    private int activeButton = 0;
+    private List<Button> buttons;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -32,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        this.buttons = new ArrayList<>();
 
         if(btAdapter != null && !btAdapter.isEnabled()){
             startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), Utils.bluetooth.ENABLE_BT_REQUEST);
@@ -41,7 +54,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initUI() {
-        findViewById(R.id.connectBtn).setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.connect_bluetooth_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
@@ -52,14 +66,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.sendBtn).setOnClickListener(new View.OnClickListener() {
+        List<Integer> ids = Arrays.asList(R.id.bt_0,R.id.bt_20,R.id.bt_40,R.id.bt_60,R.id.bt_80,R.id.bt_100);
+
+        for (final int id: ids) {
+            final Button bt = findViewById(id);
+            buttons.add(bt);
+            bt.setEnabled(false);
+            bt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(manualMode) {
+                        btChannel.sendMessage("Apertura " + bt.getText());
+                        bt.setEnabled(false);
+                        if (activeButton != 0) {
+                            findViewById(activeButton).setEnabled(true);
+                        }
+                        activeButton = id;
+                    }
+                }
+            });
+        };
+
+        Switch sw = (Switch)findViewById(R.id.manual_mode_switch);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                String message = ((EditText)findViewById(R.id.editText)).getText().toString();
-                btChannel.sendMessage(message);
-
-                ((EditText)findViewById(R.id.editText)).setText("");
-
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                manualMode = isChecked;
+                for (Button bt : buttons) {
+                    bt.setEnabled(isChecked);
+                }
+                btChannel.sendMessage("Manual: " + isChecked);
             }
         });
     }
@@ -94,13 +130,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onConnectionActive(final BluetoothChannel channel) {
 
-                ((TextView) findViewById(R.id.statusLabel)).setText(String.format("Status : connected to server on device %s",
+                ((TextView) findViewById(R.id.bluetooth_state_label)).setText(String.format("Status : connected to server on device %s",
                         serverDevice.getName()));
 
-                findViewById(R.id.connectBtn).setEnabled(false);
+                findViewById(R.id.connect_bluetooth_button).setEnabled(false);
 
                 btChannel = channel;
-                btChannel.registerListener(new RealBluetoothChannel.Listener() {
+                /*btChannel.registerListener(new RealBluetoothChannel.Listener() {
                     @Override
                     public void onMessageReceived(String receivedMessage) {
                         ((TextView) findViewById(R.id.chatLabel)).append(String.format("> [RECEIVED from %s] %s\n",
@@ -114,12 +150,12 @@ public class MainActivity extends AppCompatActivity {
                                 btChannel.getRemoteDeviceName(),
                                 sentMessage));
                     }
-                });
+                });*/
             }
 
             @Override
             public void onConnectionCanceled() {
-                ((TextView) findViewById(R.id.statusLabel)).setText(String.format("Status : unable to connect, device %s not found!",
+                ((TextView) findViewById(R.id.bluetooth_state_label)).setText(String.format("Status : unable to connect, device %s not found!",
                         Utils.bluetooth.BT_DEVICE_ACTING_AS_SERVER_NAME));
             }
         }).execute();
