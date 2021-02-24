ebc3e54f104616c9e8b706e296fc44b6b65ca281
andredecre <73247775+andredecre@users.noreply.github.com>
2021-02-24 19:25:54 +0100

    Fix server bug

--- dam_service/src/main/java/dam_service/Receiver.java
+++ dam_service/src/main/java/dam_service/Receiver.java
@@ -20,13 +20,15 @@ public class Receiver extends Thread {
 				System.out.println(msg);
 				StateEnum state = StateEnum.getStateFromValue(Integer.parseInt(msg.split(":")[0]));
 				double value = Double.parseDouble(msg.split(":")[1]);
-				long time = Long.parseLong(msg.split(":")[2]);
+				long time = System.currentTimeMillis();
 				int span = 0;
 				boolean isManual = false;
-				if(state == StateEnum.ALARM) {
-					span = Integer.parseInt(msg.split(":")[3]);
-					isManual = msg.split(":")[4] == "1" ? true : false;
+				if(state.equals(StateEnum.ALARM)) {
+					span = Integer.parseInt(msg.split(":")[2]);
+					isManual = msg.split(":")[3] == "1" ? true : false;
+							
 				}
+
 				
 				this.service.addData(new Data(time, value, state, isManual, span));
 			}
